7a46bbdc671bdd24245d6f7b1f261106bebbf98e
AlexDoninic <wenzigao@qq.com>
2019-02-14 08:56:21 +0800

    666666666666

--- BBFK/main.89def.js
+++ BBFK/main.89def.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.91b7d.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.fa377.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.91785.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.89def.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -187,12 +187,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.f4942.js');
+        require('src/settings.04df5.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.f4942.js');
+        require('src/settings.04df5.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
