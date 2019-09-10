59f6500b1d9d8e6a8f7c5acac9091263d33156fb
AlexDoninic <wenzigao@qq.com>
2019-09-10 11:57:55 +0800

    6666

--- QiangHuo/main.e098a.js
+++ QiangHuo/main.e098a.js
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.e881b.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.e098a.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -201,12 +201,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.2cb87.js');
+        require('src/settings.fc002.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.2cb87.js');
+        require('src/settings.fc002.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
