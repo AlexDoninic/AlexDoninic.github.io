a80792380ce889f99530d3a62300202f9b7434e9
AlexDoninic <wenzigao@qq.com>
2019-09-18 09:20:12 +0800

    666666

--- MSXXLUC/main.b3288.js
+++ MSXXLUC/main.b3288.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.f4d1f.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.4d140.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.21bee.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.b3288.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -201,12 +201,12 @@ else if (window.jsb) {
 
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.70c53.js');
+        require('src/settings.6c5b3.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.70c53.js');
+        require('src/settings.6c5b3.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
